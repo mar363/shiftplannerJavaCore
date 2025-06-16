030de650d6ea9f076be4abd9d1031becea47e41e
Cristina Zaharia <ncristinanedea@gmail.com>
2025-06-16 14:06:50 +0300

    Add DBUtil and DataInitializer

---
+++ src/main/java/com/demo/shiftplanner/util/DBUtil.java
@@ -0,0 +1,31 @@
+package com.demo.shiftplanner.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBUtil {
+    private static final String PROPERTIES_FILES="/application.properties";
+    private static String url;
+    private static String username;
+    private static String password;
+
+    static {
+        try(InputStream in = DBUtil.class.getResourceAsStream(PROPERTIES_FILES)) {
+            Properties props = new Properties();
+            props.load(in);
+            url = props.getProperty("db.url");
+            username = props.getProperty("db.username");
+            password = props.getProperty("db.password");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
---
+++ src/main/java/com/demo/shiftplanner/util/DatabaseInitializer.java
@@ -0,0 +1,48 @@
+package com.demo.shiftplanner.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+    public static void init() {
+        try (Connection conn = DBUtil.getConnection();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(
+                    "CREATE TABLE IF NOT EXISTS users (" +
+                    "id IDENTITY PRIMARY KEY," +
+                    "username VARCHAR(200) UNIQUE NOT NULL," +
+                    "password VARCHAR(200) NOT NULL," +
+                    "role VARCHAR(20) NOT NULL" +
+                    ")"
+            );
+
+            stmt.execute(
+                    "CREATE TABLE IF NOT EXISTS wishes (" +
+                            "id IDENTITY PRIMARY KEY," +
+                            "employee_id BIGINT NOT NULL," +
+                            "date DATE NOT NULL," +
+                            "shift_type VARCHAR(200) NOT NULL," +
+                            "CONSTRAINT fk_wish_user FOREIGN KEY(employee_id) REFERENCES users(id) ON DELETE CASCADE," +
+                            "CONSTRAINT uc_wish UNIQUE(employee_id, date)" +
+                            ")"
+            );
+
+            stmt.execute(
+                    "CREATE TABLE IF NOT EXISTS assignments (" +
+                            "id IDENTITY PRIMARY KEY," +
+                            "employee_id BIGINT NOT NULL," +
+                            "date DATE NOT NULL," +
+                            "shift_type VARCHAR(200) NOT NULL," +
+                            "CONSTRAINT fk_assign_user FOREIGN KEY(employee_id) REFERENCES users(id) ON DELETE CASCADE," +
+                            "CONSTRAINT uc_assign_employee_date UNIQUE(employee_id, date)," +
+                            "CONSTRAINT uc_assign_dat_shift UNIQUE(date, shift_type)"+
+                            ")"
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
